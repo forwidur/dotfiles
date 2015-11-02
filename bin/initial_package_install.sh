9ce0f2f671a852051daa9895880801365fefd52f
forwidur <forwidur@gmail.com>
2015-11-02 12:01:34 -0800

    Fixes double-adding of the goog repos.

--- bin/initial_package_install.sh
+++ bin/initial_package_install.sh
@@ -2,7 +2,7 @@
 
 # Adds Google repos.
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google.list'
 sudo sh -c 'echo "deb http://dl.google.com/linux/talkplugin/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 
 sudo apt-get update
