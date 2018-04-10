6baea8d75940201259e87a87918efb4d23fbbde7
Max Grigorev <forwidur@gmail.com>
2018-04-10 10:57:00 -0700

    fixup! [scripts] Adds `backup_to_server.sh.

--- bin/backup_to_server.sh
+++ bin/backup_to_server.sh
@@ -8,9 +8,8 @@ DST=${BASE}/`hostname`
 rsync --progress --copy-dirlinks -v -ar -z -e ssh papers work $DST
 
 if [ -d "/media/stuff" ]; then
-  for i in {books,audiobooks,music,music-highres}; do
-    rsync --progress --copy-dirlinks -v -a -e ssh --delete /media/stuff/${i} $DST
-  done
+  rsync --progress --copy-dirlinks -v -a -e ssh --delete \
+      /media/stuff/{books,audiobooks,music,music-highres} $DST
 fi
 
 #rsync --progress --copy-dirlinks -v -ar -e ssh /media/home/{photos,backup,books} fwd@uone.cu.cc:backups/home/store
