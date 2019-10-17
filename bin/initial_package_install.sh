577a0e70804c50aa4c4ce78df86e87f8d9964c84
Max Grigorev <forwidur@gmail.com>
2019-10-17 14:06:54 -0700

    [init] Adds undervolt.

--- bin/initial_package_install.sh
+++ bin/initial_package_install.sh
@@ -207,3 +207,6 @@ curl -s https://updates.signal.org/desktop/apt/keys.asc | sudo apt-key add -
 echo "deb [arch=amd64] https://updates.signal.org/desktop/apt xenial main" | sudo tee /etc/apt/sources.list.d/signal-xenial.list
 sudo apt update
 sudo apt install signal-desktop
+
+# Pip-based utils.
+pip3 install undervolt
