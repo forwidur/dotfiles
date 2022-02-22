2f8277047eb7ebadd7a0e59605205d982b889e5b
Max Grigorev <forwidur@gmail.com>
2022-02-22 13:59:17 -0800

    [scripts] Updates backup scripts.

--- bin/backup_to_cold.sh
+++ bin/backup_to_cold.sh
@@ -1,7 +1,22 @@
-#!/bin/sh
+#!/bin/bash
 
-LOC=/media/$USER/coldbackup
+#LOC=/media/$USER/coldbackup
+LOC=/mnt
 
-rsync -rltDvu --modify-window=1 --delete $@ work papers insoft/books bin ${LOC}/home/
-rsync -rltDvu --modify-window=1 --delete $@ /media/stuff ${LOC}/stuff
-rsync -rltDvu --modify-window=1 --delete $@ /media/home ${LOC}/store
+rsync -rltDu --modify-window=1 --delete $@ work papers insoft/books bin ${LOC}/home/
+echo "Home took ${SECONDS}s."
+SECONDS=0
+
+DEV=$(df | grep \/\$ | awk '{print $1}')
+echo "Will backup $DEV to $LOC"
+sudo dd if=$DEV bs=1M | gzip > $LOC/root.img_new.gz
+mv $LOC/root.img_new.gz $LOC/root.img.gz
+echo "Root took ${SECONDS}s,"
+SECONDS=0
+
+#rsync -rltDu --modify-window=1 --delete $@ /media/stuff ${LOC}/stuff
+#echo "Stuff took ${SECONDS}s."
+#SECONDS=0
+
+rsync -rltDu --modify-window=1 --delete $@ /media/store ${LOC}/
+echo "Store took ${SECONDS}s."
--- bin/backup_to_raid.sh
+++ bin/backup_to_raid.sh
@@ -3,5 +3,5 @@
 LOC=/media/store/
 
 cd
-rsync -a work papers bin ${LOC}/backup/home/
+rsync -a work papers bin sync ${LOC}/backup/home/
 rsync -a --exclude "movies" --exclude "music-insoft" /media/stuff/* ${LOC}/
